10b9dbe262ce349faa5e1ae4ef3aaf302387a47f
Riki Khorana <riki.singh.khorana@sony.com>
2022-04-19 00:55:34 +0900

    Fix duplicated context menu issue

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 // Copyright (c) 2022 Riki Singh Khorana. All rights reserved. MIT license.
 
-chrome.contextMenus.create({
-  id: `customDictionary-${Math.random().toString().substring(2)}`,
-  title: "Add \"%s\" to custom dictionary",
-  contexts: ["selection"],
+chrome.contextMenus.removeAll(() => {
+  chrome.contextMenus.create({
+    id: "customDictionary",
+    title: "Add \"%s\" to custom dictionary",
+    contexts: ["selection"],
+  });
 });
 
 chrome.contextMenus.onClicked.addListener((e, tab) => {
